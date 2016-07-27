5297aa0ec640285528a11e8f46ace6dee645acf0
kgrvr <kunshgrover@gmail.com>
2016-07-27 11:43:05 +0530

    Date format changed

--- app/src/main/java/com/itskush/kushal/sunshine/JSONParsing.java
+++ app/src/main/java/com/itskush/kushal/sunshine/JSONParsing.java
@@ -87,7 +87,7 @@ public class JSONParsing {
     private String getReadableDateString(long time){
         // Because the API returns a unix timestamp (measured in seconds),
         // it must be converted to milliseconds in order to be converted to valid date.
-        SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
+        SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEEE MMM dd");
         return shortenedDateFormat.format(time);
     }
 
